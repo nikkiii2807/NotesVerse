74ce13fc9f16f51009e97c2311d3cc977d7471fa
nikkiii2807 <149155165+nikkiii2807@users.noreply.github.com>
2025-01-21 13:31:29 +0530

    Update axiosInstance.js

--- frontendd/notesverse/src/utils/axiosInstance.js
+++ frontendd/notesverse/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://notesverse-backend.onrender.com',
+    baseURL: 'http://localhost:8000',
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
